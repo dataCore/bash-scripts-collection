7215436b3550ec5cc939c4098b06f7b4797b8759
datacore <datacore@geek.ch>
2025-08-21 17:29:40 +0200

    round 5

--- link.sh
+++ link.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
-folder="datacore/bash"
-cd /usr/bin || exit 1
+folder="/opt/scripts"
 
 scripts=(
-	"backup-docker.sh:backup-docker"
-	"backup-docker-all.sh:backup-docker-all"
 	"install-docker.sh:install-docker"
 	"restore-docker.sh:restore-docker"
-	"show-lastreboot.sh:show-lastreboot"
 	"update-docker.sh:update-docker"
-	"update-docker-all.sh:update-docker-all"
-	"update-scripts.sh:update-scripts"
 	"update-system.sh:update-system"
+	"update-scripts.sh:update-scripts"
+	"backup-docker.sh:backup-docker"
+	"backup-docker-all.sh:backup-docker-all"
+	"update-docker-all.sh:update-docker-all"
+	"show-lastreboot.sh:show-lastreboot"
 	"wol.sh:wol"
 )
 
 for entry in "${scripts[@]}"; do
-  	IFS=":" read -r script linkname <<<"$entry"
-  	chmod 710 "$folder/$script"
-  	ln -sf "$folder/$script" "$linkname"
+	IFS=":" read -r script linkname <<<"$entry"
+	chmod 710 "$folder/$script"
+	ln -sf "$folder/$script" "$linkname"
 done
