cfa322552d66ba9441ce10755249eb6dc2935bc0
datacore <datacore@geek.ch>
2025-08-22 10:56:30 +0200

    fix DB_PASSWORD Variables

--- backup-docker.sh
+++ backup-docker.sh
@@ -96,7 +96,7 @@ for cont in $CONTAINERS; do
             # BACKUP MariaDB
             print_status "  🐬 MariaDB: ${PROJECTNAME}.${CONTAINERNAME}.mariadbdump.sql.gz... "
             OUTPUT=${TIMESTAMP}_${PROJECTNAME}.${CONTAINERNAME}.mariadbdump.sql.gz
-            CONTAINERENV_DBPW=$(docker exec "${cont}" sh -c 'echo "$MYSQL_ROOT_PASSWORD $DB_ROOT_PASSWORD"')
+            CONTAINERENV_DBPW=$(docker exec "${cont}" sh -c 'echo "$MYSQL_ROOT_PASSWORD:-$DB_ROOT_PASSWORD"')
             docker exec "${cont}" sh -c 'exec mariadb-dump -u root -p"$0" --all-databases' "${CONTAINERENV_DBPW}" | gzip -9 >"${TEMPDIR}/${OUTPUT}"
             cp "${TEMPDIR}/${OUTPUT}" "${PROJECTBACKUPDIR}" && rm "${TEMPDIR}/${OUTPUT}"
         elif echo "$IMAGE" | grep -qi "^mysql"; then
@@ -104,7 +104,7 @@ for cont in $CONTAINERS; do
             # BACKUP MySQL
             print_status "  🐬 MySQL: ${PROJECTNAME}.${CONTAINERNAME}.mysqldump.sql.gz... "
             OUTPUT=${TIMESTAMP}_${PROJECTNAME}.${CONTAINERNAME}.mysqldump.sql.gz
-            CONTAINERENV_DBPW=$(docker exec "${cont}" sh -c 'echo "$MYSQL_ROOT_PASSWORD $DB_ROOT_PASSWORD"')
+            CONTAINERENV_DBPW=$(docker exec "${cont}" sh -c 'echo "$MYSQL_ROOT_PASSWORD:-$DB_ROOT_PASSWORD"')
             docker exec "${cont}" sh -c 'exec mysqldump -u root -p"$0" --all-databases' "${CONTAINERENV_DBPW}" | gzip -9 >"${TEMPDIR}/${OUTPUT}"
             cp "${TEMPDIR}/${OUTPUT}" "${PROJECTBACKUPDIR}" && rm "${TEMPDIR}/${OUTPUT}"
         elif echo "$IMAGE" | grep -qi "^postgres"; then
